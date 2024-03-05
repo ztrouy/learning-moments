d24d131ed89a24299e14f32f10cecd0d0b53bcb9
Zachary Trouy <ztrouy@gmail.com>
2024-03-04 21:56:58 -0600

    Added proper routing based on logged in status

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import { AllPosts } from "./components/AllPosts.jsx"
+import { Route, Routes } from "react-router-dom"
+import { ApplicationViews } from "./views/ApplicationViews.jsx"
+import { Login } from "./components/auth/Login.jsx"
+import { Register } from "./components/auth/Register.jsx"
+import { Authorized } from "./views/Authorized.jsx"
 
 export const App = () => {
-  return <AllPosts />
+  return (
+    <Routes>
+      <Route path="/login" element={<Login/>} />
+      <Route path="register" element={<Register/>} />
+      <Route path="*" element={
+        <Authorized>
+          <ApplicationViews />
+        </Authorized>
+      } />
+    </Routes>
+  )
 }

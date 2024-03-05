5ecab70cc8ddcb3ce256477359dc58452607d113
Zachary Trouy <ztrouy@gmail.com>
2024-03-04 21:57:35 -0600

    Added browser router to main render function

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { App } from "./App.jsx"
 import "./index.css"
+import { BrowserRouter } from "react-router-dom"
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
-root.render(<App />)
+root.render(<BrowserRouter><App /></BrowserRouter>)

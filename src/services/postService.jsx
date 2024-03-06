8c655d2c8e11e27bba1452d727f4809f8f941314
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-05 23:24:26 -0600

    Wrote fetches for getting a post by Id, and liking a post

--- src/services/postService.jsx
+++ src/services/postService.jsx
@@ -1,3 +1,19 @@
 export const getAllPosts = () => {
     return fetch("http://localhost:8088/posts?_expand=topic&_embed=postLikes").then(res => res.json())
 }
+
+export const getPostById = (postId) => {
+    return fetch(`http://localhost:8088/posts/${postId}?_expand=topic&_expand=user&_embed=postLikes`).then(res => res.json())
+}
+
+export const likePost = (likeObject) => {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(likeObject)
+    }
+
+    return fetch("http://localhost:8088/postLikes", postOptions)
+}

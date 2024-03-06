805e073bb31f1ff24709b0c7b7387292ed80ede6
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-05 23:42:29 -0600

    Added ability to unlike and relike a post

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -1,7 +1,7 @@
 import { Chip, Grid, Paper, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
-import { getPostById, likePost } from "../../services/postService.jsx"
+import { getPostById, likePost, modifyPostLike } from "../../services/postService.jsx"
 import { Box } from "@mui/system"
 import { Button } from "@mui/material"
 
@@ -17,7 +17,7 @@ export const PostDetails = ({ currentUser }) => {
         })
     }
 
-    const handleLike = () => {
+    const createPostLike = () => {
         const likeObject = {
             userId: currentUser.id,
             postId: post.id,
@@ -28,11 +28,22 @@ export const PostDetails = ({ currentUser }) => {
         likePost(likeObject).then(refreshPost)
     }
 
-    const handleUnlike = () => {
+    const flipIsLikedState = () => {
         const currentLikeObject = post.postLikes.find(like => like.userId === currentUser.id)
         const editedLikeObject = {...currentLikeObject}
 
-        editedLikeObject.isLiked = false
+        editedLikeObject.isLiked = !editedLikeObject.isLiked
+        editedLikeObject.date = new Date().toDateString()
+
+        modifyPostLike(editedLikeObject).then(refreshPost)
+    }
+
+    const handleLike = () => {
+        if (hasLiked === false) {
+            createPostLike()
+        } else {
+            flipIsLikedState()
+        } 
     }
 
     useEffect(() => {
@@ -80,11 +91,11 @@ export const PostDetails = ({ currentUser }) => {
                 <Grid display={"flex"} direction={"row"} marginTop={1} justifyContent={"space-between"}>
                     <Grid display={"flex"} direction={"row"} alignItems={"center"}>
                         <Typography fontWeight={"bold"} sx={{color: "#6F6F6F", marginRight: 3}}>{post.date ? post.date : ""}</Typography>
-                        <Typography textAlign={"right"} fontWeight={"bold"} sx={{color: "#6F6F6F"}}>{`${post.postLikes ? post.postLikes.length : ""} Likes`}</Typography>
+                        <Typography textAlign={"right"} fontWeight={"bold"} sx={{color: "#6F6F6F"}}>{`${post.postLikes ? post.postLikes.filter(like => like.isLiked === true).length : ""} Likes`}</Typography>
                     </Grid>
                     {currentUser.id != post.userId ? (
-                        hasLiked ? (
-                            <Button variant="contained">Unlike Post</Button>
+                        isLiked ? (
+                            <Button variant="contained" onClick={handleLike}>Unlike Post</Button>
                         ) : (
                             <Button variant="contained" onClick={handleLike}>Like Post</Button>
                         )
